3ffefff46b4227a336b6b91edcb0e7998e462efa
Tzury Bar Yochay <tzury.by@gmail.com>
2010-06-21 06:54:36 -0700

    rewriting _O in a more pythonista way

--- tornado/web.py
+++ tornado/web.py
@@ -1477,11 +1477,7 @@ def _time_independent_equals(a, b):
 
 class _O(dict):
     """Makes a dictionary behave like an object."""
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(name)
-
-    def __setattr__(self, name, value):
-        self[name] = value
+    def __new__(cls, *args, **kwargs):
+        self = dict.__new__(cls, *args, **kwargs)
+        self.__dict__ = self
+        return self
